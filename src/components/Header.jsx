13a32eea2df1b554e4b5c19982126b0f0b1a2d91
Connor <connorsmith8@gmail.com>
2022-03-20 19:10:15 -0400

    feat: add resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-
+import ArticleIcon from "@mui/icons-material/Article";
 import logo from "../assets/logo.svg";
 
 function Header() {
@@ -11,6 +11,12 @@ function Header() {
       <h2>Software Engineer</h2>
       <h3>New York, NY</h3>
       <div class="link-row">
+        <a
+          href="https://docs.google.com/document/d/e/2PACX-1vR_9RQgGeSho2wpa3J_RvUVH40JRyx9hrMHdiNbQrB9h32mTEX_Hddvu3f3KxDjS0LZ8slPiFrRVf6U/pub"
+          target="_blank"
+        >
+          <ArticleIcon />
+        </a>
         <a href="mailto: csmith8dev@gmail.com" target="_blank">
           <EmailIcon />
         </a>
